bcb098fbd38649ff0dfc6cbd705aa7864d24d477
betahikaru <beta.hikaru2000+github@gmail.com>
2015-04-04 17:07:53 +0900

    Support livedoor Weather API

--- index.ios.js
+++ index.ios.js
@@ -14,7 +14,8 @@ var {
   TouchableHighlight,
   WebView,
   ListView,
-  ActivityIndicatorIOS
+  ActivityIndicatorIOS,
+  Image,
 } = React;
 
 var RouterTest = React.createClass({
@@ -24,7 +25,7 @@ var RouterTest = React.createClass({
         style={styles.natigator}
         initialRoute={{
           component: MainListView,
-          title: 'RouterTest',
+          title: 'React Native',
         }}
         tintColor="#4A90C7"
       />
@@ -36,27 +37,94 @@ var MainListView = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <TouchableHighlight onPress={this._onPress}>
-          <Text style={styles.instructions}>
-            To get started, edit index.ios.js{'\n'}
-            Press Cmd+R to reload
-          </Text>
+        <TouchableHighlight
+          style={styles.touchable}
+          onPress={this._onPress1}>
+          <Text style={styles.instructions}>Test Data</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.touchable}
+          onPress={this._onPress2}>
+          <Text style={styles.instructions}>livedoor 天気 API (横浜)</Text>
         </TouchableHighlight>
       </View>
     );
   },
-  _onPress: function() {
+  _onPress1: function() {
     this.props.navigator.push({
       title: "Test Data",
-      // component: WebView,
-      // passProps: {url: "http://betahikaru.com"}
       component: TestDataList,
       passProps: {url: "http://betahikaru.com/testdata.json"}
     });
   },
+  _onPress2: function() {
+    this.props.navigator.push({
+      title: "Yokohama Tenki",
+      component: WeatherList,
+      passProps: {url: "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010"}
+      // http://weather.livedoor.com/weather_hacks/webservice
+    });
+  },
+});
+
+var WeatherList = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: null,
+      loaded: false
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(this.props.url)
+    .then((response) => response.json())
+    .then((responseData) => {
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.setState({
+        dataSource: ds.cloneWithRows(responseData.forecasts),
+        loaded: true
+      });
+    })
+    .done();
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicatorIOS animating={true} size='small' />
+      </View>
+    );
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) =>
+          <View style={styles.listRow}>
+            <Text style={styles.listText}>
+              {rowData.dateLabel} {rowData.telop}
+            </Text>
+            <Image
+              style={{
+                width: rowData.image.width,
+                height: rowData.image.height,
+              }}
+              source={{uri: rowData.image.url}}
+            />
+          </View>
+        }
+        style={styles.listView}
+      />
+    );
+  }
 });
 
 var TestDataList = React.createClass({
@@ -115,26 +183,29 @@ var styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-      flex: 1,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  touchable: {
+    height: 60,
+    backgroundColor: '#F5FCAA',
   },
   instructions: {
+    fontFamily: 'Hiragino Kaku Gothic ProN',
     textAlign: 'center',
     color: '#333333',
   },
   listView: {
   },
   listRow: {
-    textAlign: 'center',
-    color: '#333333',
-    height: 20,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    height: 60,
+  },
+  listText: {
+    fontFamily: 'Hiragino Kaku Gothic ProN',
   },
 });
 
